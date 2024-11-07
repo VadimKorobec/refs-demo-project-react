238e863bba9c8a673e37a61c2339996ed3f5fdf4
Vadim <_s_e_t__@ukr.net>
2024-11-07 16:46:58 +0200

    add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShow(true);
+  };
+
+  
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onClick={handleClick} />
+      {show ? <NewProject /> : <NoProjectSelected onClick={handleClick} />}
     </main>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+}
+
+const Button = ({ children, onClick }: ButtonProps) => {
+  return (
+    <button onClick={onClick} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
